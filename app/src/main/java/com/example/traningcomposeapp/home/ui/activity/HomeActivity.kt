d052ebc630bc9dcd26619255e1232810de85ee9f
Kshitij Mittal <mittalkshitij20@gmail.com>
2024-04-01 00:46:12 +0530

    006-bottom-navigation-bar-setup : bottom nav bar implemented

--- app/src/main/java/com/example/traningcomposeapp/home/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/traningcomposeapp/home/ui/activity/HomeActivity.kt
@@ -5,21 +5,41 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.example.traningcomposeapp.home.data.model.BottomNavItem
 import com.example.traningcomposeapp.home.ui.screens.HomeScreen
 import com.example.traningcomposeapp.home.ui.viewmodel.HomeViewModel
 import com.example.traningcomposeapp.ui.theme.CinePlexAppTheme
-import com.example.traningcomposeapp.utils.Constants
+import com.example.traningcomposeapp.ui.theme.TextStyleBold10
 import dagger.hilt.android.AndroidEntryPoint
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
 
 @AndroidEntryPoint
 class HomeActivity : ComponentActivity() {
@@ -31,25 +51,78 @@ class HomeActivity : ComponentActivity() {
 
         setContent {
             CinePlexAppTheme {
+                val navController = rememberNavController()
+                var selectedIconIndex by rememberSaveable {
+                    mutableIntStateOf(0)
+                }
+                val bottomNavItem = listOf(
+                    BottomNavItem.Home,
+                    BottomNavItem.Ticket,
+                    BottomNavItem.Movie,
+                    BottomNavItem.Profile
+                )
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    HomePageNavigationHandler()
+                    Scaffold(
+                        bottomBar = {
+                            NavigationBar(
+                                containerColor = MaterialTheme.colorScheme.primary
+                            ) {
+                                val navBackStackEntry by navController.currentBackStackEntryAsState()
+                                val currentDestination = navBackStackEntry?.destination
+                                bottomNavItem.forEachIndexed { index, item ->
+                                    NavigationBarItem(
+                                        colors = NavigationBarItemDefaults.colors(
+                                            selectedIconColor = MaterialTheme.colorScheme.secondary,
+                                            selectedTextColor = MaterialTheme.colorScheme.secondary,
+                                            indicatorColor = MaterialTheme.colorScheme.tertiary,
+                                            unselectedIconColor = MaterialTheme.colorScheme.onPrimary,
+                                            unselectedTextColor = MaterialTheme.colorScheme.onPrimary
+                                        ),
+                                        selected = currentDestination?.hierarchy?.any { it.route == item.route } == true,
+                                        label = {
+                                            Text(
+                                                text = item.label,
+                                                style = TextStyleBold10
+                                            )
+                                        },
+                                        onClick = {
+                                            selectedIconIndex = index
+                                            navController.navigate(item.route) {
+                                                popUpTo(navController.graph.findStartDestination().id) {
+                                                    saveState = true
+                                                }
+                                                launchSingleTop = true
+                                                restoreState = true
+                                            }
+                                        },
+                                        icon = {
+                                            Icon(
+                                                painter = painterResource(id = item.icon),
+                                                contentDescription = item.label
+                                            )
+                                        })
+                                }
+                            }
+                        }
+                    ) { innerPadding ->
+                        NavHost(
+                            navController,
+                            startDestination = BottomNavItem.Home.route,
+                            Modifier.padding(innerPadding)
+                        ) {
+                            composable(BottomNavItem.Home.route) {
+                                HomeScreen()
+                            }
+                            composable(BottomNavItem.Ticket.route) { }
+                            composable(BottomNavItem.Movie.route) { }
+                            composable(BottomNavItem.Profile.route) { }
+                        }
+                    }
                 }
             }
-        }
-    }
-}
-
-@Composable
-fun HomePageNavigationHandler() {
-
-    val navController = rememberNavController()
-    val context = LocalContext.current
-    NavHost(navController = navController, startDestination = "Home") {
-        composable(route = "Home") {
-            HomeScreen()
         }
     }
 }
